81887dcaf9c2161690318bf8250fcd41dace769a
unknown <64825962+D1urga@users.noreply.github.com>
2025-01-14 23:00:15 +0530

    updated

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,77 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Circle from "./components/circle.js";
 import { AtSymbolIcon } from "@heroicons/react/20/solid";
-import { useState } from "react";
+import { use, useState } from "react";
+import axios from "axios";
 
 export default function Home() {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [formDataRegister, setFormDataRegister] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [formDataOtp, setFormDataOtp] = useState({
+    otp: "",
+  });
   const [login, setLogin] = useState(true);
   const [otp, setOtp] = useState(false);
+  const [isLoading, setIsloading] = useState(false);
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsloading(true);
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/api/v1/user/register",
+        formDataRegister
+      );
+      setMessage(response.data.message);
+      setOtp(true);
+    } catch (error) {
+      setMessage(error.response?.data?.message || "Something went wrong");
+      console.log(error);
+    }
+    setIsloading(false);
+  };
+
+  const handleOtp = async (e) => {
+    e.preventDefault();
+    setIsloading(true);
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/api/v1/user/verify",
+        {
+          email: formDataRegister.email,
+          otp: formDataOtp.otp,
+        }
+      );
+      setMessage(response.data.message);
+      setLogin(true);
+      setOtp(false);
+    } catch (error) {
+      setMessage(error.response?.data?.message || "Something went wrong");
+      console.log(error);
+    }
+    setIsloading(false);
+  };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+  const handleChangeR = (e) => {
+    const { name, value } = e.target;
+    setFormDataRegister({ ...formDataRegister, [name]: value });
+  };
+  const handleChangeO = (e) => {
+    const { name, value } = e.target;
+    setFormDataOtp({ ...formDataOtp, [name]: value });
+  };
   return (
     <div className={styles.main}>
       {login ? (
@@ -18,10 +84,36 @@ export default function Home() {
               <p className={styles.title}>AT</p>
             </div>
           </div>
-          <input className={styles.input} placeholder="Username"></input>
-          <input className={styles.input} placeholder="Email"></input>
-          <input className={styles.input} placeholder="Password"></input>
-          <button className={styles.btn}>Login</button>
+          <input
+            className={styles.input}
+            type="text"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            required
+            placeholder="Username"
+          ></input>
+          <input
+            className={styles.input}
+            type="text"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+            placeholder="Email"
+          ></input>
+          <input
+            className={styles.input}
+            type="text"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+            placeholder="Password"
+          ></input>
+          <button className={styles.btn}>
+            {!isLoading ? "Login" : "Please wait"}
+          </button>
           <div className={styles.option}>
             <p
               className={styles.p1}
@@ -35,7 +127,7 @@ export default function Home() {
         </div>
       ) : (
         <div className={styles.div1}>
-          {otp ? (
+          {!otp ? (
             <>
               <div className={styles.ti}>
                 <div className={styles.name}>
@@ -43,10 +135,39 @@ export default function Home() {
                   <p className={styles.title}>AT</p>
                 </div>
               </div>
-              <input className={styles.input} placeholder="Username"></input>
-              <input className={styles.input} placeholder="Email"></input>
-              <input className={styles.input} placeholder="Password"></input>
-              <button className={styles.btn}>Signup</button>
+              <form onSubmit={handleSubmit} className={styles.form}>
+                {" "}
+                <input
+                  className={styles.input}
+                  type="text"
+                  name="username"
+                  value={formDataRegister.username}
+                  onChange={handleChangeR}
+                  required
+                  placeholder="Username"
+                ></input>
+                <input
+                  className={styles.input}
+                  type="text"
+                  name="email"
+                  value={formDataRegister.email}
+                  onChange={handleChangeR}
+                  required
+                  placeholder="Email"
+                ></input>
+                <input
+                  className={styles.input}
+                  type="text"
+                  name="password"
+                  value={formDataRegister.password}
+                  onChange={handleChangeR}
+                  required
+                  placeholder="Password"
+                ></input>
+                <button className={styles.btn} type="submit">
+                  {!isLoading ? "Signup" : "Please wait"}
+                </button>
+              </form>
             </>
           ) : (
             <>
@@ -56,8 +177,20 @@ export default function Home() {
                   <p className={styles.title}>AT</p>
                 </div>
               </div>
-              <input className={styles.input} placeholder="Enter otp"></input>
-              <button className={styles.btn}>Verify</button>
+              <form onSubmit={handleOtp} className={styles.form}>
+                <input
+                  className={styles.input}
+                  type="text"
+                  name="otp"
+                  value={formDataOtp.otp}
+                  onChange={handleChangeO}
+                  required
+                  placeholder="Enter otp"
+                ></input>
+                <button className={styles.btn} type="submit">
+                  {!isLoading ? "Verify" : "Please wait"}
+                </button>
+              </form>
             </>
           )}
           <div className={styles.option}>
@@ -75,6 +208,7 @@ export default function Home() {
       <div className={styles.div2}>
         <img src="bg2.svg" className={styles.img}></img>
       </div>
+      {message && <p className={styles.message}>{message}</p>}
     </div>
   );
 }
